af15b029a286a929c50c6f9c790a3e917ae8a2bd
AhmedKhaled6900 <you@example.com>
2024-03-04 21:14:21 +0200

    a

--- data/get-categories.tsx
+++ data/get-categories.tsx
@@ -13,25 +13,7 @@ const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
   };
 export default getCategories;
 
-// import useSWR from 'swr';
-// const fetcher = async (url: string | URL | Request) => {
-//   const response = await fetch(url);
-  // if (!response.ok) {
-  //   throw new Error('Failed to fetch data');
-  // }
-  // return await response.json();
-// };
-// export  const useMyData = () => {
-//   const { data, error,isLoading } = useSWR(URL, fetcher);
 
-//   return {
-//     data,
-//     isLoading: !error && !data,
-//     error,
-//   };
-// };
-
-// Define the fetcher function
 
 
 
