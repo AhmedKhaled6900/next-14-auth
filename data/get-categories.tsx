9b4c0f2fa7e76256e14a2e54a2388d70065fbf42
AhmedKhaled6900 <you@example.com>
2024-03-04 20:54:57 +0200

    try again

--- data/get-categories.tsx
+++ data/get-categories.tsx
@@ -2,17 +2,14 @@ import { Category } from "@/types";
 const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
   
  const getCategories = async (): Promise<Category[]> =>{
-
-return await fetch(URL, {
-  
- 
-  }).then((res) => res.json()).then( (data) => {
-    return  data;
-  });
- 
-
-
-
+  const data = await fetch(URL, {
+    cache: "no-cache",
+  }).then((res) => res.json());
+  return data
+// return await fetch(URL, {
+//   }).then((res) => res.json()).then( (data) => {
+//     return  data;
+//   });
   };
 export default getCategories;
 
