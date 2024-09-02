8bbf5b8dbc1cecb106305444e003c9fb3752a31d
AhmedKhaled6900 <you@example.com>
2024-09-03 00:47:53 +0300

    commit

--- actions/get-category.tsx
+++ actions/get-category.tsx
@@ -1,32 +1,22 @@
-// import { Category } from "@/types";
+import { Category } from "@/types";
 
-// const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
+const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-// const getCategory = async (id: string): Promise<Category> => {
-//   const res = await fetch(`${URL}/${id}`
+const getCategory = async (id: string): Promise<Category> => {
+  const res = await fetch(`${URL}/${id}`
   
-//   , {
-//     headers: {
-//       key: 'Content-Type',
-//       value: 'application/json',
-//     }
-//   }
-//   ).then((res) => res.json()).then((data) => {
-//     return data;
-//   });
-//   return res
+  , {
+    headers: {
+      key: 'Content-Type',
+      value: 'application/json',
+    }
+  }
+  ).then((res) => res.json()).then((data) => {
+    return data;
+  });
+  return res
 
-// };
+};
 
-// export default getCategory;
+export default getCategory;
 
-import { Category } from "@/types"
-
-const url =`${process.env.Next_PUBLIC_API_URL}/categories`
-
-const getCategories = async():Promise<Category[]>=>{
-    const res = await fetch (url)
-    return res.json()
-
-}
-export default getCategories
--- data/get-categories.tsx
+++ data/get-categories.tsx
@@ -1,23 +1,32 @@
-import { Category } from "@/types";
-const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
+// import { Category } from "@/types";
+// const URL=`${process.env.NEXT_PUBLIC_API_URL}/categories`;
   
- const getCategories = async (): Promise<Category[]> =>{
-  const data = await fetch(URL, {
-    cache: "no-cache",
-  }).then((res) => res.json());
-  return data
-// return await fetch(URL, {
-//   }).then((res) => res.json()).then( (data) => {
-//     return  data;
-//   });
-  };
-export default getCategories;
+//  const getCategories = async (): Promise<Category[]> =>{
+//   const data = await fetch(URL, {
+//     cache: "no-cache",
+//   }).then((res) => res.json());
+//   return data
+// // return await fetch(URL, {
+// //   }).then((res) => res.json()).then( (data) => {
+// //     return  data;
+// //   });
+//   };
+// export default getCategories;
 
 
 
 
 
 
+import { Category } from "@/types"
 
+const url =`${process.env.Next_PUBLIC_API_URL}/categories`
+
+const getCategories = async():Promise<Category[]>=>{
+    const res = await fetch (url)
+    return res.json()
+
+}
+export default getCategories
 
 
